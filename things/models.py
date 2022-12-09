85b58c4774f2603127eed7aab331a4e2c8135c27
@Mohammad-A-Iqbal <nea.testing.coursework@gmail.com>
2022-12-09 05:59:59 +0000

    models

--- things/models.py
+++ things/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    pass
+    name = models.CharField()
+    description = models.TimeField()
+    quantity = models.IntegerField()
